f0e776efc7f39e672bcbf95480c998354d9e3330
David Karlaš <david.karlas@gmail.com>
2014-02-16 17:08:32 +0100

    Adding ExecutionTargetsManager to game single timer across multiple projects

--- MonoDevelop.MicroFramework/MicroFrameworkExecutionTarget.cs
+++ MonoDevelop.MicroFramework/MicroFrameworkExecutionTarget.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace MonoDevelop.MicroFramework
 {
-	class MicroFrameworkExecutionTarget:ExecutionTarget
+	public class MicroFrameworkExecutionTarget:ExecutionTarget
 	{
 		public PortDefinition PortDefinition { get; private set; }
 
--- MonoDevelop.MicroFramework/MicroFrameworkProject.cs
+++ MonoDevelop.MicroFramework/MicroFrameworkProject.cs
@@ -18,88 +18,52 @@ namespace MonoDevelop.MicroFramework
 {
 	public class MicroFrameworkProject : DotNetProject
 	{
-		System.Threading.Timer timer;
 		public override void Dispose()
 		{
-			if(timer != null)
-			{
-				timer.Dispose();
-				timer = null;
-			}
+			ExecutionTargetsManager.DeviceListChanged -= OnExecutionTargetsChanged;
 			base.Dispose();
-		}
-
-		private void Init()
-		{
-			timer = new System.Threading.Timer(new System.Threading.TimerCallback(updateTargetsList), null, 0, 1000);
-		}
-		List<MicroFrameworkExecutionTarget> targetsList=new List<MicroFrameworkExecutionTarget>();
-		private void updateTargetsList(object state)
-		{
-			var devices = PortDefinition.Enumerate(PortFilter.Usb);
-			var targetsToKeep = new List<MicroFrameworkExecutionTarget>();
-			bool changed = false;
-			foreach(var device in devices)
-			{
-				bool targetExist = false;
-				foreach(var target in targetsList)
-				{
-					if(target.PortDefinition.Port == (device as PortDefinition).Port)
-					{
-						targetsToKeep.Add(target);
-						targetExist = true;
-						break;
-					}
-				}
-				if(!targetExist)
-				{
-					changed = true;
-					var newTarget = new MicroFrameworkExecutionTarget(device as PortDefinition);
-					targetsList.Add(newTarget);
-					targetsToKeep.Add(newTarget);
-				}
-			}
-			changed |= targetsList.RemoveAll((target) => !targetsToKeep.Contains(target)) > 0;
-			if(changed)
-				OnExecutionTargetsChanged();
 		}
 
 		public MicroFrameworkProject()
 			: base()
 		{
-			Init();
 		}
 
 		public MicroFrameworkProject(string languageName)
 			: base(languageName)
 		{
-			Init();
 		}
 
 		public MicroFrameworkProject(string languageName, ProjectCreateInformation projectCreateInfo, XmlElement projectOptions)
 			: base(languageName, projectCreateInfo, projectOptions)
 		{
-			Init();
+		}
+
+		protected override void OnEndLoad()
+		{
+			base.OnEndLoad();
+			if(CompileTarget != CompileTarget.Library)
+				ExecutionTargetsManager.DeviceListChanged += OnExecutionTargetsChanged;
 		}
 
 		protected override IEnumerable<ExecutionTarget> OnGetExecutionTargets(ConfigurationSelector configuration)
 		{
-			return targetsList;
+			return ExecutionTargetsManager.Targets;
 		}
 
-		public override bool SupportsFramework (TargetFramework framework)
+		public override bool SupportsFramework(TargetFramework framework)
 		{
 			return framework.Id.Identifier == ".NETMicroFramework";
 		}
 
-		public override bool SupportsFormat (FileFormat format)
+		public override bool SupportsFormat(FileFormat format)
 		{
 			return format.Id == "MSBuild10" || format.Id == "MSBuild12";
 		}
 
 		protected override bool OnGetCanExecute(ExecutionContext context, ConfigurationSelector configuration)
 		{
-			return context.ExecutionTarget is MicroFrameworkExecutionTarget;
+			return context.ExecutionTarget is MicroFrameworkExecutionTarget && base.OnGetCanExecute(context, configuration);
 		}
 
 		public override TargetFrameworkMoniker GetDefaultTargetFrameworkForFormat(FileFormat format)
