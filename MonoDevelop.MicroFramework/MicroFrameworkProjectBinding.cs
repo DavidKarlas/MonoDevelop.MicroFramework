3a2b546b4cc20f6f88838a10a3b7f3e6f5445669
David Karlaš <david.karlas@gmail.com>
2014-04-04 05:49:35 +0200

    Inheriting IProjectBinding instead of DotNetProjectBinding hopefully AddIn build will pass

--- MonoDevelop.MicroFramework/MicroFrameworkProjectBinding.cs
+++ MonoDevelop.MicroFramework/MicroFrameworkProjectBinding.cs
@@ -1,19 +1,47 @@
 using System;
 using MonoDevelop.Projects;
 using System.Xml;
+using System.IO;
 
 namespace MonoDevelop.MicroFramework
 {
-	public class MicroFrameworkProjectBinding : DotNetProjectBinding
+	public class MicroFrameworkProjectBinding : IProjectBinding
 	{
-		public override string Name
+		public string Name
 		{
 			get { return ".NETMicroFramework"; }
 		}
 
-		protected override DotNetProject CreateProject(string languageName, ProjectCreateInformation info, XmlElement projectOptions)
+		public Project CreateProject (ProjectCreateInformation info, XmlElement projectOptions)
+		{
+			string lang = projectOptions.GetAttribute ("language");
+			return CreateProject (lang, info, projectOptions);
+		}
+
+		protected DotNetProject CreateProject (string languageName, ProjectCreateInformation info, XmlElement projectOptions)
 		{
 			return new MicroFrameworkProject(languageName, info, projectOptions);
+		}
+
+		public Project CreateSingleFileProject (string file)
+		{
+			IDotNetLanguageBinding binding = LanguageBindingService.GetBindingPerFileName (file) as IDotNetLanguageBinding;
+			if (binding != null) {
+				ProjectCreateInformation info = new ProjectCreateInformation ();
+				info.ProjectName = Path.GetFileNameWithoutExtension (file);
+				info.SolutionPath = Path.GetDirectoryName (file);
+				info.ProjectBasePath = Path.GetDirectoryName (file);
+				Project project = CreateProject (binding.Language, info, null);
+				project.Files.Add (new ProjectFile (file));
+				return project;
+			}
+			return null;
+		}
+
+		public bool CanCreateSingleFileProject (string file)
+		{
+			IDotNetLanguageBinding binding = LanguageBindingService.GetBindingPerFileName (file) as IDotNetLanguageBinding;
+			return binding != null;
 		}
 	}
 }
