6ebdd9a0a86288521a8e9a387afa8552def53eca
David Karlaš <david.karlas@xamarin.com>
2014-08-01 21:58:54 +0200

    Request permission to copy framework files

--- MonoDevelop.MicroFramework/StartupHandler.cs
+++ MonoDevelop.MicroFramework/StartupHandler.cs
@@ -9,42 +9,10 @@ namespace MonoDevelop.MicroFramework
 {
 	class StartupHandler : CommandHandler
 	{
-		private static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
+		static void DirectoryCopy(string sourceDirName, string destDirName)
 		{
-			// Get the subdirectories for the specified directory.
-			DirectoryInfo dir = new DirectoryInfo(sourceDirName);
-			DirectoryInfo[] dirs = dir.GetDirectories();
-
-			if(!dir.Exists)
-			{
-				throw new DirectoryNotFoundException(
-					"Source directory does not exist or could not be found: "
-					+ sourceDirName);
-			}
-
-			// If the destination directory doesn't exist, create it. 
-			if(!Directory.Exists(destDirName))
-			{
-				Directory.CreateDirectory(destDirName);
-			}
-
-			// Get the files in the directory and copy them to the new location.
-			FileInfo[] files = dir.GetFiles();
-			foreach(FileInfo file in files)
-			{
-				string temppath = Path.Combine(destDirName, file.Name);
-				file.CopyTo(temppath, false);
-			}
-
-			// If copying subdirectories, copy them and their contents to new location. 
-			if(copySubDirs)
-			{
-				foreach(DirectoryInfo subdir in dirs)
-				{
-					string temppath = Path.Combine(destDirName, subdir.Name);
-					DirectoryCopy(subdir.FullName, temppath, copySubDirs);
-				}
-			}
+			//TNX TO: http://stackoverflow.com/a/8865284/661901
+			MonoDevelop.MacInterop.AppleScript.Run("do shell script \"cp -R \\\"" + sourceDirName + "\\\" \\\"" + destDirName + "\\\"\" with administrator privileges");
 		}
 
 		protected override void Run()
@@ -62,13 +30,13 @@ namespace MonoDevelop.MicroFramework
 
 				if(!Directory.Exists("/Library/Frameworks/Mono.framework/External/xbuild-frameworks/.NETMicroFramework"))
 				{
-					DirectoryCopy(Path.Combine(addInFolder, "files", "xbuild-framework"), "/Library/Frameworks/Mono.framework/External/xbuild-frameworks/", true);
+					DirectoryCopy(Path.Combine(addInFolder, "files", "xbuild-framework/"), "/Library/Frameworks/Mono.framework/External/xbuild-frameworks/");
 				}
 
 				if(!Directory.Exists("/Library/Frameworks/Mono.framework/Versions/Current/lib/mono/xbuild/Microsoft/.NET Micro Framework"))
 				{
-					DirectoryCopy(Path.Combine(addInFolder, "files", "xbuild"),
-						"/Library/Frameworks/Mono.framework/Versions/Current/lib/mono/xbuild/", true);
+					DirectoryCopy(Path.Combine(addInFolder, "files", "xbuild/"),
+						"/Library/Frameworks/Mono.framework/Versions/Current/lib/mono/xbuild/");
 				}
 			}
 		}
